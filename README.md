4585c98eef1b4397b437cc3dcb343b272c45cdd0
guitavano <tavano62@gmail.com>
2021-08-11 19:45:25 -0300

    first version

---
+++ README.md
@@ -0,0 +1,20 @@
+# Api do Covid-19
+<hr>
+### Projeto desenvolvido para treino, usando as requisições:
+
+* XHR
+* Fetch
+* Ajax
+
+<br>
+
+### Site que disponibiliza os dados: 
+
+[Postman](https://documenter.getpostman.com/view/10808728/SzS8rjbc#27454960-ea1c-4b91-a0b6-0468bb4e6712)
+
+<br>
+### Link para visualizar o projeto:
+
+[Covid Api]()
+
+![Imagem](./imagens/print.png)
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let confirmed = document.querySelector('#cases p')
+let deaths = document.querySelector('#deaths p')
+
+
+
 function getCurrentDate() {
     var data = new Date();
     var dia = String(data.getDate()).padStart(2, '0');
@@ -9,3 +14,79 @@ function getCurrentDate() {
 
 getCurrentDate();
 
+function apiXhr(country) {
+    let response = ""
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            response = this.responseText
+            response = JSON.parse(response)
+            let info = response[response.length - 1]
+            confirmed.textContent = parseInt(info['Confirmed']).toLocaleString('pt-BR')
+            deaths.textContent = parseInt(info['Deaths']).toLocaleString('pt-BR')
+        }
+    });
+
+    let url = "https://api.covid19api.com/total/country/" + country
+
+    xhr.open("GET", url);
+
+    xhr.send();
+
+    let countrys = document.querySelectorAll('.paises ul li')
+    for (c of countrys) {
+        c.style.color = "white"
+    }
+    document.querySelector('#' + country).style.color = "#2196f3"
+}
+
+function apiFetch(country) {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    let url = "https://api.covid19api.com/total/country/" + country
+
+    fetch(url, requestOptions)
+        .then(function (response) {
+            response.text()
+                .then(function (result) {
+                    result = JSON.parse(result)
+                    let info = result[result.length - 1]
+                    confirmed.textContent = parseInt(info['Confirmed']).toLocaleString('pt-BR')
+                    deaths.textContent = parseInt(info['Deaths']).toLocaleString('pt-BR')
+                })
+        })
+        .catch(error => console.log('error', error));
+
+    let countrys = document.querySelectorAll('.paises ul li')
+    for (c of countrys) {
+        c.style.color = "white"
+    }
+    document.querySelector('#' + country).style.color = "#2196f3"
+}
+
+function apiJquery(country) {
+    let url = "https://api.covid19api.com/total/country/" + country
+
+    var settings = {
+        "url": url,
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    $.ajax(settings).done(function (response) {
+        let info = response[response.length - 1]
+        confirmed.textContent = parseInt(info['Confirmed']).toLocaleString('pt-BR')
+        deaths.textContent = parseInt(info['Deaths']).toLocaleString('pt-BR')
+    });
+
+    let countrys = document.querySelectorAll('.paises ul li')
+    for (c of countrys) {
+        c.style.color = "white"
+    }
+    document.querySelector('#' + country).style.color = "#2196f3"
+}
